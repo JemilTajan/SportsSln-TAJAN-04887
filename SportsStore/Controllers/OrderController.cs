12208ff4ca554c1ab4053cc89e233af723045d4c
Jemil Tajan <jemiltajan5@gmail.com>
2024-05-19 00:30:41 +0800

    Not fully working. -migration error

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Controllers;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
