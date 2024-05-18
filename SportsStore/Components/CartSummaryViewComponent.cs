987c7d0ef76243034b0e0c391c45bcf9cf487900
Jemil <Jemil@LAPTOP-7TRMDIN3>
2024-05-18 23:30:15 +0800

    Add project files.

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cart) 
+        {
+            this.cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
